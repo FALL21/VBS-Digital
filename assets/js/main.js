b29ff0c13a83e01368e7a7f9663c83c40f251f4f
Fall_21 <mameboufall21@gmail.com>
2024-06-13 16:33:32 +0000

    firts

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,6 @@ let swiperPortofolio = new Swiper(".portfolio__container", {
     // el: ".swiper-pagination",
     clickabble: true,
   },
-  
 });
 /*==================== TESTIMONIAL ====================*/
 let swiperTestimonial = new Swiper(".testimonial__container", {
@@ -114,7 +113,7 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   // },
   breakpoints: {
     568: {
-      slidesPerView: 1,
+      slidesPerView: 2,
     },
   },
 });
@@ -206,38 +205,4 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-// const themeButton = document.getElementById("theme-button");
-// const darkTheme = "dark-theme";
-// const iconTheme = "uil-sun";
-
-// // Previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem("selected-theme");
-// const selectedIcon = localStorage.getItem("selected-icon");
-
-// // We obtain the current theme that the interface has by validating the dark-theme class
-// const getCurrentTheme = () =>
-//   document.body.classList.contains(darkTheme) ? "dark" : "light";
-// const getCurrentIcon = () =>
-//   themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-
-// // We validate if the user previously chose a topic
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-//     darkTheme
-//   );
-//   themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-//     iconTheme
-//   );
-// }
-
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener("click", () => {
-//   // Add or remove the dark / icon theme
-//   document.body.classList.toggle(darkTheme);
-//   themeButton.classList.toggle(iconTheme);
-//   // We save the theme and the current icon that the user chose
-//   localStorage.setItem("selected-theme", getCurrentTheme());
-//   localStorage.setItem("selected-icon", getCurrentIcon());
-// });
-/*==================== Ils nous font confiance ====================*/
+/*==================== sript pour le formulaire ====================*/
